af8955ea0a2cce30612875ea6c8332ba6a8001d1
JakeDG <32947306+JakeDG@users.noreply.github.com>
2019-05-17 16:57:44 -0400

    Added back employee.h and employeeDriver.cpp files

---
+++ OOP-Practice/OOP-Practice/employeeDriver.cpp
@@ -0,0 +1,29 @@
+/*
+	Developed by Jacob Gidley
+
+	Driver program for the Employee class
+*/
+
+#include <iostream>
+#include "employee.h"
+
+using namespace std;
+
+int main()
+{
+	// Create two employee objects
+	Employee e1("Evan", "Nelson", 60000.0);
+	Employee e2("Tasha", "Watts", 85000.0);
+
+	// Ouput current salaries
+	cout << "Current salaries:\n" << e1 << "\n" << e2 << endl;
+
+	// Adjust salaries by 10%
+	e1.adjustSalary(10.0);
+	e2.adjustSalary(10.0);
+
+	// Output adjusted salaries
+	cout << "10% adjusted salaries:\n" << e1 << "\n" << e2 << endl;
+
+	return 0;
+}
